5507bbc4fa61ed042e9773523707aa230f0302ae
Babz-G <barbaramgaynor@gmail.com>
2025-12-28 21:16:40 -0500

    added country card compnent & displayed countires on home page

---
+++ src/Components/CountryCard.jsx
@@ -0,0 +1,26 @@
+function CountryCard({ country }) {
+  return (
+    <div className="country-card">
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        className="country-flag"
+      />
+      <div className="country-info">
+        <h2>{country.name.common}</h2>
+        <p>
+          <strong>Population:</strong> {country.population.toLocaleString()}
+        </p>
+        <p>
+          <strong>Region:</strong> {country.region}
+        </p>
+        <p>
+          <strong>Capital:</strong>{" "}
+          {country.capital ? country.capital[0] : "N/A"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CountryCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
-export default function Home() {
+import localData from "../../localData.js";
+import CountryCard from "../Components/CountryCard.jsx";
+
+function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Countries!</p>
+      {localData.map((country) => (
+        <CountryCard country={country} key={country.cca3} />
+      ))}
     </div>
   );
 }
+export default Home;
